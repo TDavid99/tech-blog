951fadd7f896062fea2dcd392fd57e08f4d21093
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-10-19 11:17:56 -0400

    adding more packages and working in model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/config');
+
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+User.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4],
+            },
+        },
+    },
+)
