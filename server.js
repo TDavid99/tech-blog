6d39ab0aeb0b4f7ee5f2ad159d82bc6d1cc2ca5f
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-12 19:20:31 -0500

    main dashboard

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/config");
+const { Sequelize } = require("sequelize");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -22,3 +23,24 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({
+    helpers: {
+        format_data: data => {
+            return `${date.getMonth()+ 1}/${date.getDate()}/${date.getFullYear()}`;
+        }
+    }
+});
+
+app.engine("handlebas", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require('./controllers/'));
+
+app.listen(PORT, () =>{
+    console.log(`app listening on port ${PORT}!`);
+    Sequelize.afterSync({force: false});
+});
