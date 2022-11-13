2b21a46bd1feec112b0ca1284fcf3d4522d2c652
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-13 16:53:08 -0500

    returning homepage when users logs in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,51 @@
 const router = require("express").Router();
-const {post, User, Comment} = require("../models");
+const { User, Comment, Post} = require("../models");
 const sequelize = require("../config/connections")
 
 //homepage post
 
 router.get("/",(req, res)=>{
-    console.log()
-})
+    console.log(req.session);
+
+    Post.findAll({
+        attributes:[
+            'id',
+            'body',
+            'title',
+            'createdAt'
+        ],
+        include: [
+            {
+            model: Comment,
+            attributes:["id", "Comment_text", "post_id", "user_id"],
+            include:{
+            model: User,
+            attributes:["username"]
+            }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+         .then(dbPostData =>{
+            //single post object into homepage template
+            const posts = dbPostData.map(post.get({ plain: true}));
+           res.render("homepage", {posts, loggedIn: req.session.loggedIn});
+         })
+         .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+         });
+        });
+
+        //returns to homepage once users logs on
+        router.get("/login",(req, res) =>{
+            if(req.session.loggedIn) {
+                res.redirect('/');
+                return;
+            }
+            res.render(login);
+        });
+        
