216a24176b75da2934ca16bcc1e00b5132c2f01c
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-14 00:57:25 -0500

    working in routes dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -26,8 +26,62 @@ router.get("/", withAuth, (req,res)=>{
             },
             {
                 model: User,
-                attributes
+                attributes: ["username"]
             }
         ]
+    })
+    .then(dbPostData =>{
+        //single post object pass to homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true}));
+        res.render("homepage", {post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+// return user to homepage when they log in
+router.get("/login", (res,res) =>{
+    if(req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+    res.render("login");
+});
+
+//one post to single post page
+router.get("/post/:id", (req, res) => {
+    Post.fineUno({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id",
+            "body",
+            "title",
+            "createdAt"
+            
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "createdAt"],
+                includes: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes:['username']
+            }
+        ]
+
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({message: 'id not found'});
+            return;
+        }
     })
 })
