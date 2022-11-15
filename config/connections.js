db82820a56728702b7a48a9da28cb7e513fd6a9a
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-15 11:47:12 -0500

    working on api routes index and post

--- config/connections.js
+++ config/connections.js
@@ -10,7 +10,7 @@ if(process.env.JAWSDB_URL){
     sequelize =new Sequelize(process.env.JAWSDB_URL);
 } else{
     
-const sequelize = process.env.JAWSDB_URL
+const Sequelize = process.env.JAWSDB_URL
  new Sequelize(process.env.JAWSDB_URL,process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
