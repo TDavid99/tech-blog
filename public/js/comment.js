496a13fa4b067105017893d9a5eeaacfc85bd93e
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-15 10:20:59 -0500

    comment stringify

--- public/js/comment.js
+++ public/js/comment.js
@@ -8,5 +8,20 @@ const commentFormHandlers = async function (event){
     const comment_text = document.querySelector(
         "textarea[name='comment-body']"
     ).value;
-    
-}
+
+    if(comment_text) {
+        await fetch("/api/comment", {
+            method: "POST",
+            body: JSON.stringify({
+                post_id,
+                comment_text,
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        document.location.reload();
+    }
+};
+
+document.querySelector("#new-comment-form").addEventListener("submit", commentFormHandlers);
