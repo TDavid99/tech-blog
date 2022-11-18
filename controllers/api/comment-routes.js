930b275c94143c1db7a19c3b5feda3905f29f19e
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-17 21:23:08 -0500

    delete comment functions

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const {Comment, User , Post } = require("../../models");
+const { beforeDestroy } = require("../../models/user");
 const withAuth = require("../../utils/auth");
 
 //Get all comments
@@ -45,5 +46,63 @@ router.get("/", async (req, res) => {
     });
 
     //create a new comment
-    
+
+    router.post("/", async (req, res)=> {
+        try {
+            const dbCommentData = await Comment.create({
+                comment: req.body.commetn,
+                post_id: req.body.post_id,
+                user_id: req.body.user_id,
+            });
+
+    const comment = dbCommentData.get({plain: true});
+    res.json(comment);
+        } catch (err){
+        res.sendStatus(500)
+        }
+    });
+
+    //Update comment 
+
+    router.put("/:id", async (req, res) => {
+        try {
+            const dbCommentData = await Comment.update(
+                {
+                    comment: req.body.comment,
+                    post_id: req.body.post_id,
+                    user_id: req.body.user_id,
+                },
+                {
+                    where: {
+                        id: req.params.id,
+                    },
+                }
+            );
+            res.json(dbCommentData);
+        } catch (err) {
+             console.log(err);
+             res.sendStatus(500);
+        }
+    });
+
+    //Delete a comment
+
+    router.delete("/:id", async (req, res) => {
+        try {
+            const dbCommentData = await Comment.destroy({
+                where: {
+                    id: req.params.id,
+                },
+            });
+            if (dbCommentData) {
+                res.json(dbCommentData);
+            } else {
+                res.status(404).json({message:"Comment delete"});
+            }
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+module.exports = router;
 
