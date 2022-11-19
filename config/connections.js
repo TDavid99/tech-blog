a5992fae3831c519824d4dbd9f6cf88a4b98e922
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-19 00:04:11 -0500

    error

--- config/connections.js
+++ config/connections.js
@@ -1,20 +1,34 @@
+// const Sequelize = require('sequelize');
+
+// require('dotenv').config();
+
+
+// // let sequelize;
+// //when heroko deloy
+
+// if(process.env.JAWSDB_URL){
+//     sequelize =new Sequelize(process.env.JAWSDB_URL);
+// } else{
+    
+// const Sequelize = process.env.JAWSDB_URL
+//  new Sequelize(process.env.JAWSDB_URL,process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
+// }
+// module.exports = sequelize;
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-let sequelize;
-//when heroko deloy
-
-if(process.env.JAWSDB_URL){
-    sequelize =new Sequelize(process.env.JAWSDB_URL);
-} else{
-    
-const Sequelize = process.env.JAWSDB_URL
- new Sequelize(process.env.JAWSDB_URL,process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
-}
 module.exports = sequelize;
--- models/index.js
+++ models/index.js
@@ -1,23 +1,25 @@
-const User = require('./user');
-const Post = require('./post');
-const comment = require ('./comment');
+const User = require("./user");
+const Post = require("./post");
+const Comment = require ("./comment");
 
 Post.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: "userId",
     onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
-    foreginKey: 'postId',
+    foreginKey: "postId",
     onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User,{
+    foreginKey: "user_id",
+    onDelete: "CASCADE",
 
 });
 
 module.exports ={
     User,
     Comment,
-    Post
+    Post,
 };
